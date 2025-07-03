8ed6ec7a538fc8528dedd35c994515a864cdcdb0
Piero Delgado <piero.a.delgado@gmail.com>
2025-07-04 01:38:19 +0200

    hola mundo
    primer hola mundo

---
+++ ejemplo/holamundo.cpp
@@ -0,0 +1,12 @@
+//esto es la biblioteca estandar de entra-salida
+#include <iostream>
+
+//punto de entrada principal del programa
+int main() {
+
+    //esto imprime el texto hola mundo y da un salto de linea
+    std::cout << "¡Hola, mundo!" << std::endl;
+
+    //devuelve 0 para indicar que el programa funciono correctamente.
+    return 0;
+}
